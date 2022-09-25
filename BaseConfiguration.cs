d29226c6448197ca724a459c46a518ed6ed00e8f
Badea741 <mohammed30981710@f-eng.tanta.edu.eg>
2022-09-25 21:05:04 +0200

    Added Base Configuration

---
+++ BaseConfiguration.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CommonGenericClasses;
+public class BaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).ValueGeneratedOnAdd();
+        builder.Property(e => e.CreationDate).HasDefaultValue(DateTime.Now);
+        builder.Property(e => e.ConcurrencyStamp).IsRowVersion();
+    }
+}
